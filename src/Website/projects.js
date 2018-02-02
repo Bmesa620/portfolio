225b607fc95c99a8824e9614adf7d08de294fcf7
bmesa620 <bmesa620@gmail.com>
2018-02-02 17:09:31 -0500

    typo and color fixes

--- src/Website/projects.js
+++ src/Website/projects.js
@@ -15,7 +15,7 @@ class Projects extends Component{
             },
             brianmesa: {
                 id: "brianmesa",
-                description: "My website was a simple exercise in React, HTML and CSS in order to create a small expression of myself. I approached it using design elements and organizations that I feel allow for minimum scrolling and navigation while trying to keep things uncluttered. The color pallete I choose was inspired by colors I see everyday, such as my text editors, terminals, programs and my favorite websites.",
+                description: "My website was a simple exercise in React, HTML and CSS in order to create a small expression of myself. I approached it using design elements and organizations that I feel allow for minimum scrolling and navigation while trying to keep things uncluttered. The color palette I choose was inspired by colors I see everyday, such as my text editors, terminals, programs and my favorite websites.",
                 link: "https://github.com/Bmesa620/portfolio",
                 stack: "ReactJs | Javascript | HTML | CSS ",
                 part: ["Used React to handle most of the site, it’s a very useful library that I heavily enjoy when it comes to front-end.", "React handles most of the routing, elements and information you see on the DOM.", "Created my own style sheet using CSS and organized my HTML as best as possible.", "Created display block modals with hidden html."]
